7981b2082bc58805774d43a53b040defd8f63e4b
Alessio Farinelli <alessio.farinelli.developer@gmail.com>
2021-04-03 13:14:33 +0200

    feat(Navbar): changed breadcrumbs management

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,25 @@
 import './Navbar.css';
+import { breadcrumbs } from './models/breadcrums';
+import { useState } from 'react';
 
 export const Navbar = () => {
+
+    // state
+    const [ selected, setSelected ] = useState(0);
+    // functions
+    let onChangeSelected = index => { setSelected(index) }
+    
+    // render 
     return (
         <div className="navbar">
-            <div className="__link">
-                <a>HOME</a>
-                <div className="--underline"></div>
-            </div>
-            <div className="__link">
-                <a>BLOG</a>
-                <div className="--underline"></div>
-            </div>
-            <div className="__link">
-                <a>INFO</a>
-                <div className="--underline"></div> 
-            </div>
-            <div className="__link">
-                <a>CONTACTS</a>
-                <div className="--underline"></div>
-            </div>
+            {/* breadcrumbs */}
+            {breadcrumbs.map((item, index) => (
+                <div onClick={onChangeSelected.bind(this, index)} className={`__link ${index === selected ? '--active' : null}`} key={index}>
+                    <a>{item.label}</a>
+                    <div className="--underline"></div>
+                </div>
+            ))}
         </div>
     );
+
 }
---
+++ src/components/Navbar/models/breadcrums.js
@@ -0,0 +1,18 @@
+export const breadcrumbs = [
+    {
+        "label": "home",
+        "link": "#"
+    },
+    {
+        "label": "blog",
+        "link": "#"
+    },
+    {
+        "label": "info",
+        "link": "#"
+    },
+    {
+        "label": "contacts",
+        "link": "#"
+    }
+]
