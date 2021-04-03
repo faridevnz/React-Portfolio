efabbeb72ad62b3c7deaea33e62f48d08cf953a6
Alessio Farinelli <alessio.farinelli.developer@gmail.com>
2021-04-03 18:11:24 +0200

    feat(Card): added loading background and size for images

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,9 @@ import { cardDimentsion } from './models/card-dimensions';
 export const Card = ({ dimension = cardDimentsion.SMALL, data }) => {
     return (
         <div className={`card card--${dimension}`}>
-            <img className="__image" src={data.imagePath} alt="" />
+            <div className="__image-container">
+                <img className="__image" src={data.imagePath} alt="" />
+            </div>
             <div className="__info">
                 <div className="date">
                     {data.date}
